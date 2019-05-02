00c72f5101b8844242085da3870167cc1f564c0c
user2 <falseAccord128@gmail.com>
2019-05-03 01:22:53 +0300

    test servlet code 1.

--- src/main/java/vic/test1/CServlet1.java
+++ src/main/java/vic/test1/CServlet1.java
@@ -1,6 +1,9 @@
+package vic.test1;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 public class CServlet1 extends javax.servlet.http.HttpServlet{
 	
-	protected void 	doGet(HttpServletRequest req,	HttpServletResponse resp){
+	protected void 	doGet(HttpServletRequest req,	HttpServletResponse resp) throws java.io.IOException{
 		resp.getWriter().println("Hello From  OpenShift servlet");
 		return;
 
