4dd0b131fc2640bd032508b695d1c7551c46bf33
user2 <falseAccord128@gmail.com>
2019-05-03 12:03:55 +0300

    changed  code  before develop branch deploy integr-n  testing

--- src/main/java/vic/test1/CServlet1.java
+++ src/main/java/vic/test1/CServlet1.java
@@ -1,10 +1,17 @@
 package vic.test1;
+import java.util.Optional;
+import java.util.function.Function;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 public class CServlet1 extends javax.servlet.http.HttpServlet{
 	
 	protected void 	doGet(HttpServletRequest req,	HttpServletResponse resp) throws java.io.IOException{
-		resp.getWriter().println("Hello From  OpenShift servlet");
+		Function<String, String> f1 = (String str1)->String.format("Hello,%s", str1);
+		
+		Optional.of("OpenShift servlet").map(f1).ifPresent(resp.getWriter()::println);
+		//resp.getWriter().println("Hello From  OpenShift servlet");	
+		
 		return;
 
 	}
